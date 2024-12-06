6a8806b0be09e831d37a5fd4a10862eb1e702996
Xenia Golbraykh <karzerin@gmail.com>
2024-12-06 23:11:16 +0300

    AA1. add checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,38 @@ function App() {
       <div className={style['logo']}>
         <img src={logo} alt="logo" />
       </div>
-      <div className={style['select']}>
-        <input type="checkbox" name="all" id="all" />
-        <label htmlFor="all">Всe</label>
-        <input type="checkbox" name="without" id="without" />
-        <label htmlFor="without">Без пересадок</label>
-        <input type="checkbox" name="oneTransfer" id="oneTransfer" />
-        <label htmlFor="oneTransfer">1 пересадка</label>
-        <input type="checkbox" name="twoTransfer" id="twoTransfer" />
-        <label htmlFor="twoTransfer">2 пересадки</label>
-      </div>
-      <div className={style['tabs']}>
-        <button className={style['tabs__button']}>Самый быстрый</button>
-        <button className={style['tabs__button']}>Самый дешевый</button>
-        <button className={style['tabs__button']}>Оптимальный</button>
-      </div>
-      <div className={style['tikets-list']}>
-        <div className={style['tiket']}>tiket 1</div>
+      <div className={style['content']}>
+        <div className={style['select']}>
+          <h3 className={style['title']}>Количество пересадок</h3>
+          <div className={style['checkbox-container']}>
+            <input type="checkbox" name="all" id="all" />
+            <label htmlFor="all">Всe</label>
+          </div>
+          <div className={style['checkbox-container']}>
+            <input type="checkbox" name="without" id="without" />
+            <label htmlFor="without">Без пересадок</label>
+          </div>
+          <div className={style['checkbox-container']}>
+            <input type="checkbox" name="oneTransfer" id="oneTransfer" />
+            <label htmlFor="oneTransfer">1 пересадка</label>
+          </div>
+          <div className={style['checkbox-container']}>
+            <input type="checkbox" name="twoTransfer" id="twoTransfer" />
+            <label htmlFor="twoTransfer">2 пересадки</label>
+          </div>
+          <div className={style['checkbox-container']}>
+            <input type="checkbox" name="threeTransfer" id="twoTransfer" />
+            <label htmlFor="threeTransfer">3 пересадки</label>
+          </div>
+        </div>
+        <div className={style['tabs']}>
+          <button className={style['tabs__button']}>Самый быстрый</button>
+          <button className={style['tabs__button']}>Самый дешевый</button>
+          <button className={style['tabs__button']}>Оптимальный</button>
+          <div className={style['tikets-list']}>
+            <div className={style['tiket']}>tiket 1</div>
+          </div>
+        </div>
       </div>
     </div>
   );
