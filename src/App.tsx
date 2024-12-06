ce8e9f59d9da46afce07e8cdcbc2a005fb35bde7
Xenia Golbraykh <karzerin@gmail.com>
2024-12-06 16:11:55 +0300

    AA1. add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import style from './App.module.scss';
+import logo from './assets/img/Logo.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={style['container']}>
+      <div className={style['logo']}>
+        <img src={logo} alt="logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className={style['select']}>select</div>
+      <div className={style['tabs']}>tabs</div>
+      <div className={style['tikets-list']}>
+        <div className={style['tiket']}>tiket 1</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
