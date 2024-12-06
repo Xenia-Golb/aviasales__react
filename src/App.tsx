cec9107c4f7f9c5274da8dfbd18c58c273a1f4a8
Xenia Golbraykh <karzerin@gmail.com>
2024-12-06 17:42:48 +0300

    AA1. add font, add html

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,21 @@ function App() {
       <div className={style['logo']}>
         <img src={logo} alt="logo" />
       </div>
-      <div className={style['select']}>select</div>
-      <div className={style['tabs']}>tabs</div>
+      <div className={style['select']}>
+        <input type="checkbox" name="all" id="all" />
+        <label htmlFor="all">Всe</label>
+        <input type="checkbox" name="without" id="without" />
+        <label htmlFor="without">Без пересадок</label>
+        <input type="checkbox" name="oneTransfer" id="oneTransfer" />
+        <label htmlFor="oneTransfer">1 пересадка</label>
+        <input type="checkbox" name="twoTransfer" id="twoTransfer" />
+        <label htmlFor="twoTransfer">2 пересадки</label>
+      </div>
+      <div className={style['tabs']}>
+        <button className={style['tabs__button']}>Самый быстрый</button>
+        <button className={style['tabs__button']}>Самый дешевый</button>
+        <button className={style['tabs__button']}>Оптимальный</button>
+      </div>
       <div className={style['tikets-list']}>
         <div className={style['tiket']}>tiket 1</div>
       </div>
